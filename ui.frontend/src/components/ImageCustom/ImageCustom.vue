2b62465b175a2dac7a6a0ff061ea99c2fbfbaba6
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2022-09-06 14:33:53 -0300

    feat(ImageCustom): create a new component

---
+++ ui.frontend/src/components/ImageCustom/ImageCustom.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+const props = defineProps({
+  src: String,
+  alt: String,
+  title: String
+})
+
+onMounted(() => {
+  console.info(`The props is ${props.src} ${props.alt} ${props.title}`)
+})
+</script>
+
+<template>
+ <div class="image-content">
+   <h1>Vue Image Custom Component</h1>
+   <img :src="props.src" :alt="props.alt" :title="props.title"/>
+ </div>
+</template>
+
+<style scoped lang="scss">
+.image-content {
+
+ h1 {
+   color: green;
+ }
+
+ img {
+   height: 200px;
+   border-radius: 10px;
+ }
+}
+</style>
--- ui.frontend/src/map-components.js
+++ ui.frontend/src/map-components.js
@@ -1,6 +1,19 @@
+import ImageCustom from './components/ImageCustom/ImageCustom'
 import Text from './components/Text/Text'
 import AppPage from './components/Page/AppPage'
 import { withComponentMappingContext, MapTo } from '@mavice/aem-vue-editable-components'
+
+// ImageCustom Component Mapping
+MapTo('wknd-spa-vue/components/image-custom')(
+  ImageCustom,
+  {
+    emptyLabel: 'Image',
+    isEmpty: (props) => {
+      return !props || !props.src || props.src.trim().length < 1
+    },
+    resourceType: 'wknd-spa-vue/components/image-custom'
+  }
+)
 
 // Text Component Mapping
 MapTo('wknd-spa-vue/components/text')(
